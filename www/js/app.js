d0e51e6e729a231b0bccb347a581c7853a56e3e1
jc <jc@vys-MacBook-Air.local>
2017-03-11 08:28:18 +0000

    update on mainpage

--- www/js/app.js
+++ www/js/app.js
@@ -26,42 +26,42 @@ angular.module('starter', ['ionic', 'ngCordova'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
- 
+
   $stateProvider
   .state('map', {
     url: '/',
     templateUrl: 'templates/map.html',
     controller: 'MapCtrl'
   });
- 
+
   $urlRouterProvider.otherwise("/");
- 
+
 })
 
 .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
   var options = {timeout: 10000, enableHighAccuracy: true};
- 
+
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
- 
+
     var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
- 
+
     var mapOptions = {
       center: latLng,
       zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
- 
+
     $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
+
     // add a marker
     google.maps.event.addListenerOnce($scope.map, 'idle', function(){
-     
+
       var marker = new google.maps.Marker({
           map: $scope.map,
           animation: google.maps.Animation.DROP,
           position: latLng
-      });      
-     
+      });
+
     });
 
   }, function(error){
